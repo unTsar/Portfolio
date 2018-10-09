dad3ff2a83ed59d460ca17dfa7dd50e091ae56c8
unTsar <bycucaka@gmail.com>
2018-10-09 21:07:59 +0300

    33
    33

--- js/common.js
+++ js/common.js
@@ -1,16 +1,4 @@
 $(document).ready(function(){                                                 
-    // Owl
-    $(".owl1").owlCarousel({
-        nav: true,
-        loop: true,
-        items: 1,
-    });
-    $(".owl2").owlCarousel({
-        nav: true,
-        loop: true,
-        items: 4,
-        margin: 25
-    });
 
     $('.hideB').hide();
     $a = $('.visB');
@@ -26,3 +14,46 @@ $(document).ready(function(){
     });
 
 });
+
+
+$('.owl1').owlCarousel({
+    loop: true,
+    items: 1,
+    nav: false,
+    dots: true,
+    mouseDrag: false,
+    touchDrag: false
+});
+var i= 1; $('.owl-dot').each(function(){$(this).find('span').html(i);i++;});
+$('.owl2').owlCarousel({
+    nav: false,
+    loop: true,
+    items: 4,
+    mouseDrag: false,
+    touchDrag: false
+});
+$('#prev1').click(function () {
+    $('.owl1').trigger('prev.owl.carousel', [500]);
+})
+$('#next1').click(function () {
+    $('.owl1').trigger('next.owl.carousel', [500]);
+})
+$('#prev2').click(function () {
+    $('.owl2').trigger('prev.owl.carousel', [500]);
+})
+$('#next2').click(function () {
+    $('.owl2').trigger('next.owl.carousel', [500]);
+})
+$('#dot1').click(function () {
+    $('.owl1').trigger('to.owl.carousel', [0, 500])
+})
+$('#dot2').click(function () {
+    $('.owl1').trigger('to.owl.carousel', [1, 500])
+})
+$('#dot3').click(function () {
+    $('.owl1').trigger('to.owl.carousel', [2, 500])
+})
+$('#dot4').click(function () {
+    $('.owl1').trigger('to.owl.carousel', [3, 500])
+})
+
